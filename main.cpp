fa300479ba2e3de4dad6d5b811f8168aac848f1a
rjworks <arjayandal05@yahoo.com>
2021-01-21 16:33:50 -0600

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
-#include <cstdio>
 #include <ctime>
 #include <cstdlib>
+
 using namespace std;
-void test();
 
 int main() {
     string examples[] = {"5*20", "1+2", "6/2", "3-2"};
 
     srand(time(nullptr));
     int s = rand() % 3 + 1;
+
     cout << "Type an expression." << endl;
     cout << "i.e. " + examples[s] << endl;
     string expression;
     cin >> expression;
     cout << expression;
-    return 0;
-}
 
-void test() {
-    for (int i = 0; i < 3; ++i) {
-        std::cout << i;
-    }
+    return 0;
 }
 
 
